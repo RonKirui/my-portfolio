5474f23cc7a51af42166d785c3f052c1a9203006
= <=>
2025-11-14 12:20:37 -0500

    feat: adding new features and styling blog page

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,3 +1,26 @@
+import data from "../data/blog.json";
+import like_ic from "../assets/like_ic.png";
+import comment_ic from "../assets/comment_ic.png";
 export default function BlogCard() {
-  return <div></div>;
+  return data.projects.map((project) => (
+    <div className="card-container bg-w" key={project.id}>
+      <div className="card">
+        <img src={project.image} className="" alt={project.title}></img>
+      </div>
+      <div className="card-blog-text">
+        <h5>
+          <a>{project.title}</a>
+        </h5>
+        <h6>{project.description}</h6>
+      </div>
+      <div className="action-section">
+        <a href="" target="_blank">
+          <img src={like_ic}></img>
+        </a>
+        <a href="" target="_blank">
+          <img src={comment_ic}></img>
+        </a>
+      </div>
+    </div>
+  ));
 }
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,9 +1,35 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import search_ic from "../assets/search.png";
+import BlogCard from "../components/BlogCard";
 
 export default function Blog() {
   return (
     <div className="blog-container">
-      <h1>Blog</h1>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="blog-navbar-brand">
+            <img src={logo}></img>
+          </a>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="" type="submit">
+              <img className="search-ic" src={search_ic}></img>
+            </button>
+          </form>
+        </div>
+      </nav>
+      <div className="blog-content-section">
+        <h1>Blog</h1>
+        <div className="blog-content-section-elements">
+          <BlogCard />
+        </div>
+      </div>
     </div>
   );
 }
